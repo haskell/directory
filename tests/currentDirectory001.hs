91d6a82800ce5fd57d3f776b8265e9242cf1b174
Ian Lynagh <igloo@earth.li>
2008-06-11 18:08:44 +0000

    Make currentDirectory001 not conflict with other tests running in parallel

--- tests/currentDirectory001.hs
+++ tests/currentDirectory001.hs
@@ -1,15 +1,18 @@
-import System.Directory (getCurrentDirectory, setCurrentDirectory, 
-                     createDirectory, removeDirectory, getDirectoryContents)
 
+import System.Directory (getCurrentDirectory, setCurrentDirectory,
+                         createDirectory, removeDirectory,
+                         getDirectoryContents)
+
+main :: IO ()
 main = do
     oldpwd <- getCurrentDirectory
-    createDirectory "foo"
-    setCurrentDirectory "foo"
+    createDirectory dir
+    setCurrentDirectory dir
     ~[n1, n2] <- getDirectoryContents "."
     if dot n1 && dot n2 
      then do
         setCurrentDirectory oldpwd
-        removeDirectory "foo"
+        removeDirectory dir
         putStr "Okay\n"
       else
         ioError (userError "Oops")
@@ -18,3 +21,7 @@ dot :: String -> Bool
 dot "." = True
 dot ".." = True
 dot _ = False
+
+dir :: FilePath
+dir = "currentDirectory001-dir"
+
