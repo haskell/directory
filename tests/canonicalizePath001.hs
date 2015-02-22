abfa69b28b55ff2dd2b541f8c881b028b960cc7e
Elliot Robinson <elliot.robinson@argiopetech.com>
2015-02-22 15:46:11 -0500

    Cleans up canonicalizePath

--- tests/canonicalizePath001.hs
+++ tests/canonicalizePath001.hs
@@ -1,28 +1,10 @@
 module Main(main) where
 
-import Control.Concurrent
-import Control.Monad
 import Control.Exception
 import System.Directory
-import System.FilePath
-import System.IO.Error
 
 main = do
   dot <- canonicalizePath "."
   nul <- (canonicalizePath "")
            `catch` ((\_ -> return "") :: IOException -> IO String)
-  print $ dot == nul
-
-report :: Show a => IO a -> IO ()
-report io = do
-  r <- try io
-  case r of
-   Left e  -> print (e :: SomeException)
-   Right a -> print a
-
-ignore :: IO a -> IO ()
-ignore io = do
-  r <- try io
-  case r of
-   Left e  -> let _ = e :: SomeException in return ()
-   Right a -> return ()
+  print (dot == nul)
