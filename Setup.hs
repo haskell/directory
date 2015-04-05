079da19a4596f2477de3519f77cd84e4acf90610
Phil Ruffwind <rf@rufflewind.com>
2015-04-05 03:19:00 -0400

    Include 'python' and 'which' as build-tools for tests
    This is needed to make sure the Nix package works correctly.

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,8 @@
 module Main (main) where
 
 import Distribution.Simple
+import Distribution.Simple.Program (simpleProgram)
 
 main :: IO ()
 main = defaultMainWithHooks autoconfUserHooks
+       { hookedPrograms = [simpleProgram "python", simpleProgram "which"] }
