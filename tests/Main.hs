d619c51d72212103e3a8972280ca37ac141fc759
Phil Ruffwind <rf@rufflewind.com>
2015-05-29 20:40:10 -0400

    Migrate test: T8482

--- tests/Main.hs
+++ tests/Main.hs
@@ -2,8 +2,10 @@ module Main (main) where
 import qualified Util as T
 import qualified CanonicalizePath
 import qualified FileTime
+import qualified T8482
 
 main :: IO ()
 main = T.testMain $ \ _t -> do
   T.isolatedRun _t "CanonicalizePath" CanonicalizePath.main
   T.isolatedRun _t "FileTime" FileTime.main
+  T.isolatedRun _t "T8482" T8482.main
--- tests/T8482.hs
+++ tests/T8482.hs
@@ -1,16 +1,21 @@
+{-# LANGUAGE CPP #-}
+module T8482 where
+#include "util.inl"
+import GHC.IO.Exception (IOErrorType(InappropriateType))
 import System.Directory
-import Control.Exception
+import System.IO.Error (ioeGetErrorType)
 
+tmp1 :: FilePath
 tmp1 = "T8482.tmp1"
+
+testdir :: FilePath
 testdir = "T8482.dir"
 
-main = do
+main :: TestEnv -> IO ()
+main _t = do
   writeFile tmp1 "hello"
   createDirectory testdir
-  tryRenameFile testdir tmp1 >>= print  -- InappropriateType
-  tryRenameFile tmp1 testdir >>= print  -- InappropriateType
-  tryRenameFile tmp1 "." >>= print  -- InappropriateType
-  removeDirectory testdir
-  removeFile tmp1
-  where tryRenameFile :: FilePath -> FilePath -> IO (Either IOException ())
-        tryRenameFile opath npath = try $ renameFile opath npath
+  T(expectIOErrorType) () (is InappropriateType) (renameFile testdir tmp1)
+  T(expectIOErrorType) () (is InappropriateType) (renameFile tmp1    testdir)
+  T(expectIOErrorType) () (is InappropriateType) (renameFile tmp1    ".")
+  where is t = (== t) . ioeGetErrorType
