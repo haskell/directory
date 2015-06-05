7d5c1eb6de40513a78c509f92307a232fd2c1d5c
Phil Ruffwind <rf@rufflewind.com>
2015-06-04 21:14:31 -0400

    Migrate test: Directory001

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,11 +1,13 @@
 module Main (main) where
 import qualified Util as T
 import qualified CanonicalizePath
+import qualified Directory001
 import qualified FileTime
 import qualified T8482
 
 main :: IO ()
 main = T.testMain $ \ _t -> do
   T.isolatedRun _t "CanonicalizePath" CanonicalizePath.main
+  T.isolatedRun _t "Directory001" Directory001.main
   T.isolatedRun _t "FileTime" FileTime.main
   T.isolatedRun _t "T8482" T8482.main
