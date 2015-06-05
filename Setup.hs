f539cc5c34ada4d48bcab171a818259cd1af94bb
Phil Ruffwind <rf@rufflewind.com>
2015-06-05 03:19:03 -0400

    Remove files associated with old test framework

--- Setup.hs
+++ Setup.hs
@@ -1,8 +1,6 @@
 module Main (main) where
 
 import Distribution.Simple
-import Distribution.Simple.Program (simpleProgram)
 
 main :: IO ()
 main = defaultMainWithHooks autoconfUserHooks
-       { hookedPrograms = [simpleProgram "python", simpleProgram "which"] }
