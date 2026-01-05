df997ff6375fd87f35bf148aa64fc8d910936ba0
Phil Ruffwind <rf@rufflewind.com>
2026-01-04 18:27:51 -0800

    Add custom linter and also HLint

--- tools/lints.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eux
-
-version=`sed -n 's/^version:[[:space:]]*//p' directory.cabal`
-grep -q "## $version (" changelog.md || {
-    echo >&2 "Error: Please describe version $version in changelog.md."
-    exit 1
-}
